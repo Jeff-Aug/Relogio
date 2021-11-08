0426458139597748fdc08bdb1776fad0e5a68b17
Jeff-Aug <dentyn_nyt@outlook.com>
2021-11-08 20:35:13 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-let section = document.querySelector("section")
-let icons= document.querySelector("section")
+
+let section = document.querySelector("section"),
+icons = document.querySelector(".icons");
+icons.onclick = ()=>{
+    section.classList.toggle("dark");
+}
+
+
+
+// configunrando a hora
+setInterval(()=>{
+
+    let date = new Date(),
+    hour = date.getHours(),
+    min = date.getMinutes(),
+    sec = date.getSeconds();
+
+    let d;
+    d = hour < 12 ? "AM" : "PM"; 
+    hour = hour > 12 ? hour - 12 : hour; 
+    hour = hour == 0 ? hour = 12 : hour; 
+
+    
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    // modificando a DOM
+    document.querySelector(".hour_num").innerText = hour;
+    document.querySelector(".min_num").innerText = min;
+    document.querySelector(".sec_num").innerText = sec;
+    document.querySelector(".am_pm").innerText = d;
+
+  }, 1000); // 1000 milliseconds = 1s
